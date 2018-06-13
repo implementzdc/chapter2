f45c3d26fb74257a76acf266d61e83faa4b79350
zhuduancheng <494764792@qq.com>
2018-06-14 00:02:07 +0800

    新增一个user实体类

---
+++ src/main/java/com/cc/domain/User.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: User
+ * Author:   zt
+ * Date:     2018/6/14 0:00
+ * Description: 用户实体类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.cc.domain;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈用户实体类〉
+ *
+ * @author 朱端成
+ * @create 2018/6/14
+ * @since 1.0.0
+ */
+public class User {
+}
