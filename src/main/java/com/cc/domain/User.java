35dca1df66cc046c438aa9cff585a1cc95971c77
zhuduancheng <494764792@qq.com>
2018-06-14 17:20:15 +0800

    增加User中的字段

--- src/main/java/com/cc/domain/User.java
+++ src/main/java/com/cc/domain/User.java
@@ -19,4 +19,31 @@ package com.cc.domain;
  * @since 1.0.0
  */
 public class User {
+    private String id ;
+    private String last_login_time ;
+    private String last_login_ip ;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLast_login_time() {
+        return last_login_time;
+    }
+
+    public void setLast_login_time(String last_login_time) {
+        this.last_login_time = last_login_time;
+    }
+
+    public String getLast_login_ip() {
+        return last_login_ip;
+    }
+
+    public void setLast_login_ip(String last_login_ip) {
+        this.last_login_ip = last_login_ip;
+    }
 }
